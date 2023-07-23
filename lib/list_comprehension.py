582c8d8064ecec308eca40f143213886f82badfc
Michael Mathias <michaelscodingjourney@gmail.com>
2023-07-22 20:47:21 -0700

    Used list comprehension to pass tests

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [number for number in num_list if number % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + '!' for sentence in sentence_list]
